8242b2357a9339cbb54709836e5c621544942796
CSMahajan <chaitanya.mahajan13@gmail.com>
2024-01-24 13:24:43 +0530

    Initial Commit

--- src/main/java/com/parcel/service/CostCalculatorService.java
+++ src/main/java/com/parcel/service/CostCalculatorService.java
@@ -9,17 +9,29 @@ import org.springframework.stereotype.Service;
 public class CostCalculatorService {
 
     public CostCalculatorStrategy calculateCostDetailsForParcel(ParcelDetails parcelDetails) {
-        if (parcelDetails.getWeight() > ParcelConstraints.HEAVY_PARCEL_WEIGHT_LIMIT) {
+        if (isHeavyParcel(parcelDetails)) {
             return new HeavyParcelCostCalculatorStrategy();
         } else {
             double volume = parcelDetails.getHeight() * parcelDetails.getLength() * parcelDetails.getWidth();
-            if (volume < ParcelConstraints.SMALL_PARCEL_VOLUME_LIMIT) {
+            if (isSmallParcel(volume)) {
                 return new SmallParcelCostCalculatorStrategy(volume);
-            } else if (volume >= ParcelConstraints.SMALL_PARCEL_VOLUME_LIMIT && volume < ParcelConstraints.LARGE_PARCEL_VOLUME_LIMIT) {
+            } else if (isMediumParcel(volume)) {
                 return new MediumParcelCostCalculatorStrategy(volume);
             } else {
                 return new LargeParcelCostCalculatorStrategy(volume);
             }
         }
+    }
+
+    private static boolean isHeavyParcel(ParcelDetails parcelDetails) {
+        return parcelDetails.getWeight() > ParcelConstraints.HEAVY_PARCEL_WEIGHT_LIMIT;
+    }
+
+    private static boolean isSmallParcel(double volume) {
+        return volume < ParcelConstraints.SMALL_PARCEL_VOLUME_LIMIT;
+    }
+
+    private static boolean isMediumParcel(double volume) {
+        return volume >= ParcelConstraints.SMALL_PARCEL_VOLUME_LIMIT && volume < ParcelConstraints.LARGE_PARCEL_VOLUME_LIMIT;
     }
 }
